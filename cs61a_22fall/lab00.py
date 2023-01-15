e839bd681806a20d08965b523cdc342b7a1cec4f
flying-tangerine <112827101+flying-tangerine@users.noreply.github.com>
2023-01-15 23:15:35 +0100

    Add files via upload

---
+++ cs61a_22fall/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty_two():
+    """ This is a docstring. And the following is a doctest.
+    Come up with the most creative expression that evaluates to 2022, using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_two()
+    2022
+    """
+    a = 200 * 10 + 20 + 2
+    return a
